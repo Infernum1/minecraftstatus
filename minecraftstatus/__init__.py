7bf40f35cfdb5d512f2a482be9fbd035ad6e0193
oBreez <81740112+oBreez@users.noreply.github.com>
2022-03-02 11:10:42 +0530

    revert init

--- minecraftstatus/__init__.py
+++ minecraftstatus/__init__.py
@@ -1,9 +1,8 @@
 from .client import *
-from .server_status import *
-from .errors import *
 
 __version__ = "0.0.7"
 __author__ = "Infernum1"
 __title__ = "minecraftstatus"
 __copyright__ = "Copyright (c) 2022 Infernum1"
 __summary__ = "minecraftstatus is an asynchronous wrapper for https://api.iapetus11.me."
+__all__ = ("MCStatus", "HTTPClient", "ServerStatus", "ServerNotFound", "BadTextFormation")
