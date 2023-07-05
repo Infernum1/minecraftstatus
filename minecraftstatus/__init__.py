50651d09ec16135fd25b6e408c8d7a9c82549980
Infernum11 <81740112+Infernum1@users.noreply.github.com>
2023-07-05 20:01:41 +0530

    change year

--- minecraftstatus/__init__.py
+++ minecraftstatus/__init__.py
@@ -5,6 +5,6 @@ from .server_status import *
 __version__ = "0.0.7"
 __author__ = "Infernum1"
 __title__ = "minecraftstatus"
-__copyright__ = "Copyright (c) 2022 Infernum1"
+__copyright__ = "Copyright (c) 2023 Infernum1"
 __summary__ = "minecraftstatus is an asynchronous wrapper for https://api.iapetus11.me."
 __all__ = ("MCStatus", "HTTPClient", "ServerStatus", "ServerNotFound", "BadTextFormation")
