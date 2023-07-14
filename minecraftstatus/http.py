4ee5ded8574f87b7ece2a4898190115e55c61c2c
Infernum11 <81740112+Infernum1@users.noreply.github.com>
2023-07-14 22:03:16 +0530

    improve HTTPClient class

--- minecraftstatus/http.py
+++ minecraftstatus/http.py
@@ -7,11 +7,25 @@ class HTTPClient:
     def __init__(self):
         self.session = None
 
-    async def _request(self, method: str, endpoint: str) -> dict:
+    async def open(self):
         self.session = ClientSession()
-        resp = await self.session.request(method, endpoint)
-        return resp
 
-    async def _close(self):
+    async def close(self):
         if self.session:
             await self.session.close()
+
+        self.session = None
+
+    async def _request(self, method: str, endpoint: str) -> dict:
+        if self.session:
+            resp = await self.session.request(method, endpoint)
+            return resp
+
+        raise RuntimeError("There was no open session found. Call .open() or use this class as a context manager")
+
+    async def __aenter__(self):
+        await self.open()
+        return self
+
+    async def __aexit__(self, *args, **kwargs):
+        await self.close()
